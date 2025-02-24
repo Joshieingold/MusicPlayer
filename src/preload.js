a44f86ce35c81e6e105af98a78e9bd9c719b4b2f
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2025-02-24 18:13:07 -0400

    some nice updates

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,22 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge } = require('electron');
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+let currentChoice = 0;
+
+contextBridge.exposeInMainWorld("api", {
+    ChangeSong: () => {
+        const img = document.getElementById("img");
+        if (!img) return; // Prevents errors if the element doesn't exist
+
+        let newChoice;
+        do {
+            newChoice = randomInt(0, 4);
+        } while (newChoice === currentChoice);
+
+        currentChoice = newChoice;
+        img.src = `MusicArt/${currentChoice}.gif`;
+    }
+});
